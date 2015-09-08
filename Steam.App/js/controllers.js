84da779267e315e31fd906a3c763faea3ed6375c
anthony-butcher <anthony.b@mmtdigital.co.uk>
2015-09-08 15:23:21 +0100

    Weapon filters

--- Steam.App/js/controllers.js
+++ Steam.App/js/controllers.js
@@ -4,14 +4,17 @@ var config = {
 };
 
 var data = {
-    games: [{
-        name: 'Counter Strike: Gobal Offensive',
-        id: 730
-    },
-    {
-        name: 'Another test game',
-        id: 12
-    }]
+    games: [
+        {
+            name: 'Counter Strike: Gobal Offensive',
+            id: 730
+        },
+        {
+            name: 'Another test game',
+            id: 12
+        }
+        ],
+    weapons: ['ak47','m4a1','awp','hkp2000','p250','deagle','p90','fiveseven','glock','negev','famas','ssg08','tec9','galilar','bizon','aug','sawedoff','hegrenade','elite','mp7','mag7','ump45','nova','xm1014','knife','sg556','mp9','mac10','m249','scar20','g3sg1','molotov','taser']
 };
 
 steamApp.controller('ProfileCtrl', function ($scope, $http) {
@@ -26,8 +29,10 @@ steamApp.controller('ProfileCtrl', function ($scope, $http) {
         }
     };
 
+    $scope.weapons = data.weapons;
     $scope.steamId = '76561197969877387';
     $scope.statFilter = '';
+    $scope.statWeaponFilter = '';
     $scope.statOrderProperty = 'name';
     $scope.statOrder = '';
 
@@ -40,5 +45,12 @@ steamApp.controller('ProfileCtrl', function ($scope, $http) {
     $scope.getOrder = function() {
         return $scope.statOrder + $scope.statOrderProperty;
     };
+
+    $scope.getFilter = function() {
+        var separator = ($scope.statFilter.length === 0) ? '' : ' ',
+            filter = $scope.statFilter + separator + $scope.statWeaponFilter;
+
+        return filter;
+    }
 
 });
