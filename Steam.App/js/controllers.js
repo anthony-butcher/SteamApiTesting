0e3681120250be0aebe8de35d6f019f062f6ebcd
anthony-butcher <anthony.b@mmtdigital.co.uk>
2015-09-08 16:34:52 +0100

    Started separating out different controllers into separate routes and added simple node static file handler.

--- Steam.App/js/controllers.js
+++ Steam.App/js/controllers.js
@@ -1,8 +1,3 @@
-var steamApp = angular.module('steamApp', []);
-var config = {
-    baseUrl: 'http://localhost:'
-};
-
 var data = {
     games: [
         {
@@ -13,12 +8,32 @@ var data = {
             name: 'Another test game',
             id: 12
         }
-        ],
+    ],
     weapons: ['ak47','m4a1','awp','hkp2000','p250','deagle','p90','fiveseven','glock','negev','famas','ssg08','tec9','galilar','bizon','aug','sawedoff','hegrenade','elite','mp7','mag7','ump45','nova','xm1014','knife','sg556','mp9','mac10','m249','scar20','g3sg1','molotov','taser']
 };
 
-steamApp.controller('ProfileCtrl', function ($scope, $http) {
+var steamApp = angular.module('steamApp', ['ngRoute']);
 
+steamApp.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/player', {
+            templateUrl: 'partials/player.html',
+            controller: 'PlayerController'
+        })
+        .when('/player/:steamId', {
+            templateUrl: 'partials/player.html',
+            controller: 'PlayerController'
+        })
+        .when('/another', {
+            templateUrl: 'partials/another.html',
+            controller: 'AnotherController'
+        })
+        .otherwise({
+            redirectTo: 'player'
+        });
+}]);
+
+steamApp.controller('PlayerController', function ($scope, $http, $routeParams) {
     var service = {
         getCsgoStats: function(steamId, callback) {
             $http
@@ -29,12 +44,9 @@ steamApp.controller('ProfileCtrl', function ($scope, $http) {
         }
     };
 
-    $scope.weapons = data.weapons;
-    $scope.steamId = '76561197969877387';
-    $scope.statFilter = '';
-    $scope.statWeaponFilter = '';
-    $scope.statOrderProperty = 'name';
-    $scope.statOrder = '';
+    $scope.submitHandler = function(steamId) {
+        location.href = '/#/player/' + steamId;
+    };
 
     $scope.getStats = function(steamId) {
         service.getCsgoStats(steamId, function(data) {
@@ -47,10 +59,27 @@ steamApp.controller('ProfileCtrl', function ($scope, $http) {
     };
 
     $scope.getFilter = function() {
-        var separator = ($scope.statFilter.length === 0) ? '' : ' ',
+        var separator = ($scope.statFilter.length !== 0 && $scope.statWeaponFilter.length !== 0) ? ' ' : '',
             filter = $scope.statFilter + separator + $scope.statWeaponFilter;
 
         return filter;
     }
+
+    $scope.weapons = data.weapons;
+    $scope.statFilter = '';
+    $scope.statWeaponFilter = '';
+    $scope.statOrderProperty = 'name';
+    $scope.statOrder = '';
+
+    if($routeParams.steamId) {
+        $scope.steamId = $routeParams.steamId;
+        $scope.getStats($routeParams.steamId);
+    }
+});
+
+
+steamApp.controller('AnotherController', function($scope) {
+
+    console.log('AnotherController');
 
 });
